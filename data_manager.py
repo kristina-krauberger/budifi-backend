a019d8db46c0fbe81cc1287d97b57232b3960062
Kristina Krauberger <krauberger.kristina@gmail.com>
2025-11-10 12:34:39 +0100

    Moved user data fetching logic to DataManager and replaced deprecated query.get() with session-based execution

--- data_manager.py
+++ data_manager.py
@@ -1,6 +1,8 @@
+from sqlalchemy import select
 from models.course import Course
 from models.user import User
 from models.enrollment import Enrollment
+from datetime import datetime
 
 class DataManager:
 
@@ -19,3 +21,36 @@ class DataManager:
             return f"{e}"
 
 
+    def get_current_user_dashboard(self, user_id):
+        """Fetches all user-related dashboard data, including personal info, enrolled courses, and progress."""
+
+        # Fetches personal data of current user for greeting and identification in dashboard.
+        user = self.db.session.execute(select(User).where(User.id==user_id)).scalar_one()
+        first_name = user.first_name
+
+        # Fetches enrollment data, including courses, progress and enrollment date for the current user.
+        enrollment = self.db.session.execute(select(Enrollment).filter_by(user_id=user_id)).scalars().all()
+
+        # Calculates how many days have passed since the current user first enrolled.
+        enrolled_at = enrollment[0].enrolled_at
+        days_since_enrolled = (datetime.utcnow() - enrolled_at).days
+
+        # Builds a list of all courses with their progress data for current user.
+        courses_data = []
+        for entry in enrollment:
+            course = self.db.session.execute(select(Course).where(Course.id==entry.course_id)).scalar_one()
+            progress = entry.progress
+            courses_data.append({
+                "course_id": course.id,
+                "course_name": course.course_name,
+                "progress": progress
+            })
+
+        # Collects all relevant data of current user for display to the dashboard in a dictionary.
+        user_dashboard_data = {
+            "user_id": user.id,
+            "first_name": first_name,
+            "days_since_enrolled": days_since_enrolled,
+            "courses": courses_data
+        }
+        return user_dashboard_data
